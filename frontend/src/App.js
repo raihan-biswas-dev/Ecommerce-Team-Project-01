29394dd18ed9da9f06401d838efd65e6e54a4b72
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2022-05-10 00:26:03 +0600

    kam shesh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import { Container, Navbar, Nav, Row, Col, Card, Button } from 'react-bootstrap'
-import data from './data';
+import { Container, Navbar, Nav} from 'react-bootstrap'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Products from './components/Products';
@@ -28,27 +27,6 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-
-
-      <Container >
-        <Row>
-          {data.map(item => (
-            <Col lg={3}>
-              <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={item.img} />
-                <Card.Body>
-                  <Card.Title>{item.name}</Card.Title>
-                  <Card.Title>${item.price}</Card.Title>
-                  <Card.Text>
-                    {item.carddescription}
-                  </Card.Text>
-                  <Button className='addTocartBtn' variant="primary">Add to cart</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
     </div>
   );
 }
--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
-function Products () {
+import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import data from '../data'
+function Products() {
     return (
         <div>
-            <h1>Products</h1>
+            <Container >
+                <Row>
+                    {data.map(item => (
+                        <Col lg={3}>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={item.img} />
+                                <Card.Body>
+                                    <Card.Title>{item.name}</Card.Title>
+                                    <Card.Title>${item.price}</Card.Title>
+                                    <Card.Text>
+                                        {item.carddescription}
+                                    </Card.Text>
+                                    <Button className='addTocartBtn' variant="primary">Add to cart</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </div>
     )
 }
