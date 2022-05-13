c386bd8cf3dc3a5a38372e296127a3415fdac7a6
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2022-05-13 14:58:11 +0600

    some error fixed

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,66 +1,72 @@
 import React, { useContext, useEffect, useState } from 'react'
-import {Container, Form } from 'react-bootstrap'
+import { Container, Form, Alert, Button } from 'react-bootstrap'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { Store } from '../Store'
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
 
 const Login = () => {
 
   const navigate = useNavigate()
-  let {search,state} = useLocation()
+  let { search, state } = useLocation()
   let redirectUrl = new URLSearchParams(search).get('redirect')
   let redirect = redirectUrl ? redirectUrl : "/"
 
-  let [email,setEmail]=useState("")
-  let [password,setPassword]=useState("")
+  let [email, setEmail] = useState("")
+  let [password, setPassword] = useState("")
 
-  const {state3,dispatch3} = useContext(Store)
-  const {userInfo} = state3
+  const { state3, dispatch3 } = useContext(Store)
+  const { userInfo } = state3
 
 
-  let handleSubmit=async(e)=>{
+  let handleSubmit = async (e) => {
     e.preventDefault()
-    try{
-      const {data} =await axios.post("/api/users/signin",{
+    try {
+      const { data } = await axios.post("/api/users/signin", {
         email,
         password
       })
       // console.log(data)
-      dispatch3({type:'USER_SIGNIN',payload:data})
-      localStorage.setItem('userInfo',JSON.stringify(data))
+      dispatch3({ type: 'USER_SIGNIN', payload: data })
+      localStorage.setItem('userInfo', JSON.stringify(data))
       navigate(redirect || "/")
-    }catch(err){
+    } catch (err) {
       toast.error("Invalid E-mail or Password")
     }
 
   }
 
-  useEffect(()=>{
-    if(userInfo){
-      navigate (redirect)
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect)
     }
-  },[])
+  }, [])
 
   return (
     <>
-    
-      <Container>
-      <Form style={{maxWidth:"600px",margin:"0 auto"}} onSubmit={handleSubmit} >
-  <Form.Group className="mb-3">
-    <Form.Control onChange={(e)=>setEmail(e.target.value)} className='signinforminpu1' type="email" placeholder="Enter email" />
-  </Form.Group>
 
-  <Form.Group className="mb-3" >
-    <Form.Control onChange={(e)=>setPassword(e.target.value)} className='signinforminpu2' type="password" placeholder="Password" />
-  </Form.Group>
-  <button className='sing-button' variant="primary" type="submit">
-    Sign in
-  </button>
-  <Form.Text>
-  Not registered? <Link to={`/signup?redirect=${redirect}`}>SIgn up</Link>
-  </Form.Text>
-</Form>
+      <Container className='w-25 mt-5 border loginForm p-3'>
+        <Alert variant="secondary" className='text-center'>
+          <h2>Login</h2>
+        </Alert>
+
+
+
+        <Form onSubmit={handleSubmit} >
+          <Form.Group className="mb-3">
+            <Form.Control onChange={(e) => setEmail(e.target.value)} className='signinforminpu1' type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" >
+            <Form.Control onChange={(e) => setPassword(e.target.value)} className='signinforminpu2' type="password" placeholder="Password" />
+          </Form.Group>
+          <Button className='loginBtn' variant="primary" type="submit">
+            Sign in
+          </Button>
+          <Form.Text>
+            Not registered? <Link to={`/signup?redirect=${redirect}`}>SIgn up</Link>
+          </Form.Text>
+        </Form>
       </Container>
     </>
   )
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Button, Container, Form } from 'react-bootstrap'
+import { Button, Container, Form, Alert } from 'react-bootstrap'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { Store } from '../Store'
@@ -13,12 +13,12 @@ const Signup = () => {
   let redirectUrl = new URLSearchParams(search).get("redirect")
   let redirect = redirectUrl ? redirectUrl : "/"
 
-  let [name,setName]=useState("")
-  let [email,setEmail]=useState("")
-  let [password,setPassword]=useState("")
-  let [confirmpassword,setConfirmpassword]=useState("")
+  let [name, setName] = useState("")
+  let [email, setEmail] = useState("")
+  let [password, setPassword] = useState("")
+  let [confirmpassword, setConfirmpassword] = useState("")
 
-  
+
   const { state3, dispatch3 } = useContext(Store)
 
 
@@ -27,61 +27,67 @@ const Signup = () => {
   let handlesignupSubmit = async (e) => {
     e.preventDefault()
     try {
-        const { data } = await axios.post("/api/users/signup", {
-            name,
-            email,
-            password
-        })
-        console.log(data)
+      const { data } = await axios.post("/api/users/signup", {
+        name,
+        email,
+        password
+      })
+      console.log(data)
 
-        dispatch3({ type: 'USER_SIGNIN', payload: data })
-        localStorage.setItem('userInfo', JSON.stringify(data))
-        navigate(redirect || '/')
+      dispatch3({ type: 'USER_SIGNIN', payload: data })
+      localStorage.setItem('userInfo', JSON.stringify(data))
+      navigate(redirect || '/')
     } catch (err) {
-        toast.error("Invalid email or pass")
+      toast.error("Invalid email or pass")
     }
-}
-
-
-useEffect(()=>{
-  if(userInfo){
-    navigate (redirect)
   }
-},[])
+
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect)
+    }
+  }, [])
 
 
   return (
     <>
 
-  <Container>
-      <Form style={{maxWidth:"600px",margin:"0 auto",marginTop:"150px"}} onSubmit={handlesignupSubmit} >
+      <Container className='w-25 mt-5 border p-3 signupForm'>
+        <Alert variant="secondary" className='text-center'>
+          <h2>Sign Up</h2>
+        </Alert>
 
-  <Form.Group className="mb-3">
-    <Form.Label  >Name</Form.Label>
-    <Form.Control className='signinforminpu1' onChange={(e)=>setName(e.target.value)}  type="text" placeholder="write your name" />
-  </Form.Group>
-  <Form.Group className="mb-3">
-    <Form.Label  >Email address</Form.Label>
-    <Form.Control className='signinforminpu1' onChange={(e)=>setEmail(e.target.value)}  type="email" placeholder="Enter email" />
-  </Form.Group>
 
-  <Form.Group className="mb-3">
-    <Form.Label>Password</Form.Label>
-    <Form.Control className='signinforminpu1' onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3">
-    <Form.Label>Confirm Password</Form.Label>
-    <Form.Control className='signinforminpu1' onChange={(e)=>setConfirmpassword(e.target.value)} type="password" placeholder="Confirm Password" />
-  </Form.Group>
-  <button className='sing-button' variant="primary" type="submit">
-    Sign Up
-  </button>
-  <Form.Text>
-  Already Have An Account? <Link to={`/signin?redirect=${redirect}`}>Log In</Link>
-  </Form.Text>
-</Form>
+
+        <Form onSubmit={handlesignupSubmit} >
+
+          <Form.Group className="mb-3">
+            <Form.Label  >Name</Form.Label>
+            <Form.Control className='signinforminpu1' onChange={(e) => setName(e.target.value)} type="text" placeholder="write your name" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label  >Email address</Form.Label>
+            <Form.Control className='signinforminpu1' onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control className='signinforminpu1' onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control className='signinforminpu1' onChange={(e) => setConfirmpassword(e.target.value)} type="password" placeholder="Confirm Password" />
+          </Form.Group>
+          <Button className='sing-button' variant="primary" type="submit">
+            Sign Up
+          </Button>
+          <Form.Text>
+            Already Have An Account? <Link to={`/signin?redirect=${redirect}`}>Log In</Link>
+          </Form.Text>
+        </Form>
       </Container>
-      
+
     </>
   )
 }
